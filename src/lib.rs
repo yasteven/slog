86c3a9d65fbf61bc7ad599af0153c146b55253dd
Steven E. Elliott <see@thefractaltennis.com>
2026-06-21 01:18:44 -0400

    slog first

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #[macro_export]
 macro_rules! error
 { ($n:expr, $($args:tt)*) => 
-  { { static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
+  { { const POSITIVE : u64 = $n;
+      if POSITIVE <= 0
+      {// This doesn't work  compile_error!("slog::error!(call_number, \"// must be 1 or more \" " );
+      }
+    
+      static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
       let call_number = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
       if (call_number % $n) == 0 
       { log::error!("[{}] {}", call_number, format!($($args)*));
@@ -12,7 +17,12 @@ macro_rules! error
 #[macro_export]
 macro_rules! info
 { ($n:expr, $($args:tt)*) => 
-  { { static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
+  { { const POSITIVE : u64 = $n;
+      if POSITIVE <= 0
+      {// This doesn't work  compile_error!("slog::info!(call_number, \"// must be 1 or more \" " );
+      }
+    
+      static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
       let call_number = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
       if (call_number % $n) == 0 
       { log::info!("[{}] {}", call_number, format!($($args)*));
@@ -23,7 +33,12 @@ macro_rules! info
 #[macro_export]
 macro_rules! debug
 { ($n:expr, $($args:tt)*) => 
-  { { static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
+  { { const POSITIVE : u64 = $n;
+      if POSITIVE <= 0
+      {// This doesn't work when i had POSITIVE : i32 to try to do thing compile_error!("slog::debug!(call_number, \"// must be 1 or more \" " );
+      }
+    
+      static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
       let call_number = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
       if (call_number % $n) == 0 
       { log::debug!("[{}] {}", call_number, format!($($args)*));
@@ -34,7 +49,12 @@ macro_rules! debug
 #[macro_export]
 macro_rules! trace
 { ($n:expr, $($args:tt)*) => 
-  { { static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
+  { { const POSITIVE : u64 = $n;
+      if POSITIVE <= 0
+      { // This doesn't work compile_error!("slog::trace!(call_number, \"// must be 1 or more \" " );
+      }
+    
+      static COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
       let call_number = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
       if (call_number % $n) == 0 
       { log::trace!("[{}] {}", call_number, format!($($args)*));
